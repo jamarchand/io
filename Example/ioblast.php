2b634aafc72c061034be9c4df27727f9306870bb
jamarchand <JorgeMarchand@Jorge-Marchands-MacBook-Pro-2.local>
2018-02-20 00:19:47 -0800

    Untrimmed Clusters
    This is the introductory set of package to generate un-trimmed trees.
    Still need to add the rest of the trimming package.

---
+++ Example/ioblast.php
@@ -0,0 +1,114 @@
+
+<?php
+    //Assigning locus: Using the protein list to asign an ordered locus to each protein. (Note: Locus assignment is an approximation. Fully assembled genomes will have only errors at terminal loci, while partially assembled genomes will have errors between non-overlapping contigs.
+    function find_locus($fname, $protM){
+        $i = 0;
+        $handle = fopen($fname, "r");
+        if ($handle) {
+            while (($line = fgets($handle)) !== false) {
+                $exprot = explode('WP',$line);
+                $prot = 'WP'.substr($exprot[1],0,10);
+                $i = $i + 1;
+                if ($protM == $prot){
+                    return $i;
+                    break;
+                }
+            }
+            fclose($handle);
+        } else {
+            print_r("error opening the file.");
+        }
+    }
+    
+    
+    ///Matching: Trimming the blast results to only include WP accession for each protein and finding the locus for each alignment.
+    function match_prot($fname, $dl){
+        $i = 0;
+        $handle = fopen($fname, "r");
+        $assigned= fopen("locus.csv", "w");
+        if ($handle) {
+            while (($line = fgets($handle)) !== false) {
+                
+                //Find protA
+                $exprot = explode('WP',$line);
+                if (sizeof($exprot)==3){ //There should be exactly 3 lines for 2 proteins
+                    $st = strpos($line, $dl);
+                    
+                    $protA = 'WP'.substr($exprot[1],0,10); //Get protein A
+                    $locusA = find_locus('tables/cattleya-table.txt',$protA); //Get protein locus
+                    $protB = 'WP'.substr($exprot[2],0,10); //Get protein B
+                    $locusB = find_locus('tables/catenulae-table.txt',$protB); //Get protein B locus
+
+                    $protloc = $protA.','.$protB.','.$locusA.','.$locusB."\n";
+                    fwrite($assigned, $protloc);
+
+                    print_r($protA.' '.$protB.' '.$locusA.' '.$locusB);
+                    print_r("\n");
+                }
+            }
+            fclose($handle);
+        } else {
+            print_r("error opening the file.");
+        }
+        fclose($assigned);
+    }
+    
+    
+    
+    ///Removing duplicates and order list
+    function remove_dup($fname){
+        $csv = array_map('str_getcsv', file($fname));
+        $deduped= fopen("unique-locus.csv", "w");
+        for ($i = 0; $i<10000; $i ++){
+            for ($ii = 0; $ii<10000; $ii ++){
+                if ($csv[$ii][2] == $i ){
+                    $protloc = $csv[$ii][0].','.$csv[$ii][1].','.$csv[$ii][2].','.$csv[$ii][3]."\n";
+                    fwrite($deduped, $protloc);
+                    break;
+                    }
+            }
+            }
+        fclose($deduped);
+    }
+    
+    
+    
+    ///Clustering dataset based on pairwise distance
+    function cluster_set($fname, $dist, $thr){
+        $csv = array_map('str_getcsv', file($fname));
+        $clusters = fopen("clusters-untrimmed.csv", "w");
+        for ($i = 0; $i<=(sizeof($csv)-$dist); $i ++){
+
+            for ($ii = 1; $ii<=$dist; $ii ++){
+                $dx = $csv[$i][3]-$csv[$i+$ii][3]; //Calculate distance between proteins
+                if (abs($dx)<$thr){ //Threshold based on distance and generate cluster file
+                    $pwdist = $csv[$i][0].','.$csv[$i+$ii][0].','.$dx."\n";
+                    fwrite($clusters, $pwdist);
+                }
+            }
+        }
+        fclose($clusters);
+    }
+    
+    
+    ////Local database: Making local blast database of the in-group.
+    print_r('Status: Generating in-group database'); print_r("\n");
+    //shell_exec("makeblastdb -in genomes/catenulae.fasta -out databases/databaseBLAST -dbtype prot -parse_seqids");
+    
+    
+    //Blasting ingroup: Blasting database against query organism (ex: S. cattleya)
+    print_r('Status: Blasting to generate ingroup'); print_r("\n");
+    //shell_exec("blastp -query genomes/cattleya.fasta -db databases/databaseBLAST -outfmt 6 -num_alignments 1 -out ingroup.txt");
+    
+    
+    print_r('Status: Matching sets and assigning loci'); print_r("\n");
+    //match_prot('ingroup.txt','_prot'); //Delimiter is specific to S. cattleya. Chose most appropriate here.
+    
+    print_r('Status: Trimming duplicate and ordering set'); print_r("\n");
+    //remove_dup('locus.csv'); //Remove duplicates.
+    
+    print_r('Status: Clustering based on distance threashold'); print_r("\n");
+    cluster_set('unique-locus.csv', 2, 10);
+    
+    
+    ?>
---
+++ ReadMe.txt
@@ -0,0 +1,40 @@
+Comparative genomics proof-of-concept developed by Jorge Marchand
+This software package aided in the discovery of terminal-alkyne amino acid found in Streptomyces spp. 
+
+============================= From =============================
+
+(Discovery of a pathway for terminal-alkyne amino acid biosynthesis) 
+Authors:  J. A. Marchand1, M. E. Neugebauer1, C. Lin2, J. Pelton3, M. C. Y. Chang2,4*
+Affiliations:1Department of Chemical and Biomolecular Engineering, University of California, Berkeley, CA 94720-14602Department of Chemistry, University of California, Berkeley, CA 94720-14603QB3 Institute, University of California, Berkeley, CA 94720, USA 4Department of Molecular and Cell Biology, University of California, Berkeley, CA 94720-3200
+============================= ioBlast =============================
+ioBlast concept usage below. This set of software is not optimized, and meant to be used as a learning tool.
+Please read through code carefully to make necessary adjustments for personal case use. Example provided 
+should be able to replicate results shown in “Discovery of a pathway for terminal-alkyne amino acid biosynthesis”
+
+
+ioBlast.php takes 6 inputs: 
+-Target organism proteome fasta file
+-Query organism proteome fasta file
+-Ordered set of protein loci for target organism
+-Ordered set of protein loci for query organism
+-Loci pairwise distance to calculate 
+-Clustering distance threshold 
+
+
+ioBlast.php generates many temporary files but has one main output: 
+-set of clustered proteins shared between two organisms (cytoscape ready .csv file) 
+
+============================= ioTrim =============================
+ioTrim is used to trim output file from ioBlast to remove clusters that are shared by your target organisms 
+and a database of outgroup organisms. The more outgroup organisms you chose, the more possible candidate clusters 
+you remove from cluster-space. However, using organisms in the out-group that contain a cluster of interest will result in 
+trimming of that cluster from the cluster-space (giving a false negative). Careful attention should be paid towards which 
+organisms are used for in-group and which are used for out-group. 
+
+
+ioTrim.php takes 1 input: 
+-Untrimmed .csv file output from ioBlast.php 
+
+ioTrim.php has 1 output: 
+-Trimmed .csv file (cityscape ready) 
+
